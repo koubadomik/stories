f22e7c322de83d124691f1a15a44ae5e045cfb74
Dominik Kouba <31382955+koubadomik@users.noreply.github.com>
2021-04-09 00:18:04 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,4 @@
 sed -n 11p complete_histogram | cut -d "[" -f2 | cut -d "]" -f1 | tr -s , \\n | tr -s '=>' , > histogram.csv
+
+#json schema
+sudo jq 'leaf_paths | join(".")' report.json | cut -d . -f1 | uniq
