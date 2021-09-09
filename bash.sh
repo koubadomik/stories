5f97a62be4da7554477015fed1870e662adeb836
koubadom <dominik.kouba@resistant.ai>
2021-09-09 22:57:33 +0200

    name refactor

--- bash.sh
+++
@@ -1,5 +0,0 @@
-sed -n 11p complete_histogram | cut -d "[" -f2 | cut -d "]" -f1 | tr -s , \\n | tr -s '=>' , > histogram.csv
-
-#json schema
-sudo jq 'leaf_paths | join(".")' report.json | cut -d . -f1 | uniq #first level
-sudo jq 'leaf_paths | join(".")' report.json | cut -d . -f1,2 | uniq #second level
